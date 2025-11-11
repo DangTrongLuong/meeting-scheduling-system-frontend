62dd64a5d77d52844ac942fd6fc7c6a9e050c46f
yrcoksuk <quangdoleminh@gmail.com>
2025-11-11 17:00:10 +0700

    Resolve conflict và thêm CalendarPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Auth/Register.jsx";
 import LoginPage from "./pages/Auth/Login.jsx";
 import DashboardUser from "./pages/User/DashboardUser.jsx";
 import Profile from "./pages/Auth/Profile.jsx";
 import Room from "./pages/Admin/Room/RoomsList.jsx";
+import CalendarPage from "./pages/CalendarPage.jsx";
+import VerifyPage from "./pages/Verify/VerifyPage.jsx";
+import ForgotPasswordPage from "./pages/Auth/ForgotPassword.jsx";
+import DashboardAdmin from "./pages/Admin/DashboardAdmin.jsx";
+import Devices from "./pages/Admin/Device/Devices.jsx";
 
 import { UserProvider } from "./context/UserContext.jsx";
 import AuthMiddleware from "./middlewares/AuthMiddleware.jsx";
 import "./App.css";
-import VerifyPage from "./pages/Verify/VerifyPage.jsx";
-import ForgotPasswordPage from "./pages/Auth/ForgotPassword.jsx";
- 
-import DashboardAdmin from "./pages/Admin/DashboardAdmin.jsx";
-import Devices from "./pages/Admin/Device/Devices.jsx";
- 
+
 function App() {
   return (
     <Router>
       <UserProvider>
         <Routes>
+          {/* Auth routes */}
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<LoginPage />} />
           <Route path="/verify" element={<VerifyPage />} />
-          <Route path="/forgot_password" element={<ForgotPasswordPage />} />   
-          <Route path="/managementRooms" element={<AuthMiddleware><Room /></AuthMiddleware>} />
+          <Route path="/forgot_password" element={<ForgotPasswordPage />} />
 
+          {/* Protected routes */}
+          <Route
+            path="/managementRooms"
+            element={
+              <AuthMiddleware>
+                <Room />
+              </AuthMiddleware>
+            }
+          />
           <Route
             path="/loginSuccess"
             element={<AuthMiddleware>{null}</AuthMiddleware>}
@@ -38,7 +46,6 @@ function App() {
               </AuthMiddleware>
             }
           />
- 
           <Route
             path="/profile"
             element={
@@ -47,7 +54,6 @@ function App() {
               </AuthMiddleware>
             }
           />
- 
           <Route
             path="/dashboardAdmin"
             element={
@@ -64,10 +70,18 @@ function App() {
               </AuthMiddleware>
             }
           />
+          <Route
+            path="/calendar"
+            element={
+              <AuthMiddleware>
+                <CalendarPage />
+              </AuthMiddleware>
+            }
+          />
         </Routes>
       </UserProvider>
     </Router>
   );
 }
- 
+
 export default App;
